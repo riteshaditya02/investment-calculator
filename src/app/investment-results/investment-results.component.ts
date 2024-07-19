263e7e5c7ec4c4ff95ae02e48fdaea991199631a
Ritesh Aditya <ritesh.aditya@invincix.com>
2024-07-19 11:55:05 +0530

    First Commit

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,16 @@
+import { Component, input, Input } from '@angular/core';
+import { InvestmentResult } from '../investment-input.model';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+
+  @Input() results?:InvestmentResult[];
+
+}
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input.model';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+  enteredInitialInvestment = '0';
+  enteredAnualInvestment = '0';
+  enteredExpectedReturn  = '3';
+  enteredDuration = '1';
+
+  onSubmit(){
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enteredAnualInvestment
+    })
+
+  }
+
+}
